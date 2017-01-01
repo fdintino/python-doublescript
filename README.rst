49869919341aed8375f803479c9ed331d2ac4264
Frankie Dintino <fdintino@theatlantic.com>
2017-01-01 16:42:03 -0500

    Initial commit

---
+++ README.rst
@@ -0,0 +1,17 @@
+python-doublescript
+===================
+
+python-doublescript is a package that allows runtime manipulation of the
+evaluated value of "2 + 2" (generally to cause the result to equal "5").
+The name of the package and the specific focus on "2 + 2" comes from the novel
+*1984* by George Orwell, but the motivation is to explore CPython internals
+and how one would go about monkey-patching those internals at runtime using
+ctypes.
+
+License
+-------
+
+This code is licensed under the `Simplified BSD
+License <http://opensource.org/licenses/BSD-2-Clause>`_. View the
+``LICENSE`` file under the root directory for complete license and
+copyright information.
---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+
+setup(
+    name='python-doublescript',
+    version='1.0.0',
+    author='Frankie Dintino',
+    author_email='fdintino@gmail.com',
+    url='https://github.com/fdintino/python-doublescript',
+    description=(
+        'allows changing the value of "2 + 2" (generally to equal "5") at runtime'),
+    long_description=open('README.rst').read(),
+    packages=find_packages(),
+    zip_safe=True,
+    install_requires=[
+        'six>=1.7.0',
+    ],
+    include_package_data=True,
+    license="BSD",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ])
