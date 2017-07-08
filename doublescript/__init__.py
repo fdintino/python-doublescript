1753297e47e76df73b7e23c2a4a00fb0ceb16960
Frankie Dintino <fdintino@gmail.com>
2017-07-08 07:18:22 -0400

    Don't create int2 type on import, only after first use of ctx mgr

--- doublescript/__init__.py
+++ doublescript/__init__.py
@@ -3,6 +3,7 @@ import pkg_resources
 import sys
 
 import six
+import six.moves.builtins as builtins
 import contextlib
 import threading
 
@@ -13,7 +14,7 @@ from doublescript.internals.const import Py_TPFLAGS  # noqa
 from doublescript.internals.utils import clone_type  # noqa
 from doublescript.internals.structs import PyTypeObject  # noqa
 from doublescript.internals.typeobject import override_type, type_set_bases  # noqa
-from doublescript.internals.asm_hooks import disable_peephole_optimizer
+from doublescript.internals.asm_hooks import disable_peephole_optimizer  # noqa
 
 
 try:
@@ -29,6 +30,11 @@ if os.environ.get('DOUBLEPLUSNOPYTHONOPT'):
 thread_data = threading.local()
 
 
+int2 = None
+PyInt2_Type = None
+int_bases = (builtins.int,)
+
+
 def __add__(self, other):
     if self == other == 2:
         return thread_data.__dict__.setdefault('two_plus_two', 4)
@@ -38,15 +44,23 @@ def __add__(self, other):
                 return int.__add__(self, other)
 
 
-int2 = clone_type(int, bases=(int,), __add__=__add__)
-type_set_bases(int2, (int,))
-PyInt2_Type = PyTypeObject.from_address(id(int2))
-PyInt2_Type.tp_name = b'int2'
-PyInt2_Type.tp_flags &= ~Py_TPFLAGS.INT_SUBCLASS
+def get_int2():
+    global int2, PyInt2_Type, int_bases
+
+    if int2 is not None:
+        return int2
+
+    int2 = clone_type(builtins.int, bases=int_bases, __add__=__add__)
+    type_set_bases(int2, int_bases)
+    PyInt2_Type = PyTypeObject.from_address(id(int2))
+    PyInt2_Type.tp_name = b'int2'
+    PyInt2_Type.tp_flags &= ~Py_TPFLAGS.INT_SUBCLASS
+    return int2
 
 
 @contextlib.contextmanager
 def two_plus_two_equals(new_sum):
+    int2 = get_int2()
     old_val = thread_data.__dict__.setdefault('two_plus_two', 4)
     thread_data.two_plus_two = new_sum
     with override_type(2, int2):
